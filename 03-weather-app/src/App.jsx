87cbb294310f56f4272eb3d12fbc9aded1b74a89
Felipe Carneiro <felipedosssantos896@gmail.com>
2022-06-07 14:55:37 -0300

    criando o componente card e sua estrutura

--- 03-weather-app/src/App.jsx
+++ 03-weather-app/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex flex-col w-full h-screen items-center justify-center">
       <form>
         <input className="p-3 rounded-lg outline-none" type="text" placeholder="Cidade" />
         <button className="bg-blue-600 p-3 rounded-lg ml-3 text-white font-bold" type="submit">Pesquisar</button>
       </form>
+
+      <Card />
     </div>
   );
 }
---
+++ 03-weather-app/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <span>Nome da cidade</span>
+        <span>Estado</span>
+        <span>País</span>
+      </div>
+
+      <div>
+        <span>27</span>
+        <span>C</span>
+      </div>
+
+      <div>
+        <span>icone</span>
+        <span>nublado</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
