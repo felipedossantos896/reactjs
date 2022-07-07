4a565db53c1b31908e00217027e0abaddc2212f2
Felipe Carneiro <felipedosssantos896@gmail.com>
2022-07-07 09:43:10 -0300

    [feat]: update button component

--- 02-conometrador-tarefas/src/components/Botao/index.tsx
+++ 02-conometrador-tarefas/src/components/Botao/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 class Botao extends React.Component {
   render() {
+      const estaAtivo = false;
+      const styles = {
+        backgroundColor: estaAtivo ? "green" : "red"
+      }
+
       return(
-        <button>Botão</button>
+        <button style={styles}>Botão</button>
       )
   }
 }
